94afb41b9305d7a8686afa019c10033ec2f7e1cf
xiaoyu-iid <xiaoyu.shi@outlook.edu>
2016-11-29 20:01:19 -0500

    added ScoreBoardLose, scaled game UI

--- javascript/level_1.js
+++ javascript/level_1.js
@@ -6,7 +6,7 @@ var level_1State = {
 		this.bgSound = game.add.audio('music');
 
 		//command picture
-		this.cm = game.add.sprite(10, 500 ,'command');
+		this.cm = game.add.sprite(10, 500 , 'command');
 		//what does this mean
 		//this.cm.anchor.setTo ('10', '90');
 
@@ -20,8 +20,8 @@ var level_1State = {
 		//the spped of the shapes when generating
 		this.minSpeed = -1;
 		this.maxSpeed = 2;
-		this.vx = Math.random()*(this.maxSpeed - this.minSpeed+1) - this.minSpeed;
-		this.vy = Math.random()*(this.maxSpeed - this.minSpeed+1) - this.minSpeed;
+		this.vx = Math.random()*(this.maxSpeed - this.minSpeed + 1) - this.minSpeed;
+		this.vy = Math.random()*(this.maxSpeed - this.minSpeed + 1) - this.minSpeed;
 
 		this.messageGameOver = '';
 
@@ -30,19 +30,28 @@ var level_1State = {
 		this.heartsGroup = game.add.group();
 
 		this.win = true;	
+		this.messageGameOver = "You win!";
 		this.scoreBoardGroup = game.add.group();
+
+		//can change the style of characters later
+		this.scorelabel = game.add.text(30,25,'Numbers: 0', {front : "10px Arial",
+															 fill: "#ffffff"
+															});
+
+		this.lifelabel = game.add.text(game.world.width / 2 + 275, 25, 'Your Lifes: ',{front : '10px Arial',
+																					  fill: '#ffffff'});
 
 		//creating the three hearts in the group and render on the screen
 		for (var i = 0; i < this.lifes; i++){
-			this.hearts = this.heartsGroup.create(game.world.width / 2 + 300 +(i * 50), 25, "heart");
+			this.hearts = this.heartsGroup.create(game.world.width / 2 + 420 + (i * 45), 25, "heart");
 			this.hearts.scale.setTo(0.2, 0.2);
 
 		}
 
 		//timer
-		game.time.events.add(15000, this.gameOver, this);
-		timeRemaining = 15;
-		timeRemainingText = game.add.text(30, 60, 'Time remaining:' + "15s", {
+		game.time.events.add(30000, this.gameOver, this);
+		timeRemaining = 30;
+		timeRemainingText = game.add.text(30, 60, "Time remaining:" + "30s", {
 			font: "20px Arial",
         	fill: "#ffffff",
         	align: "left"	
@@ -51,10 +60,7 @@ var level_1State = {
 		//call the function createShapes to create the shapes
 		game.time.events.loop(1000, this.createShapes, this);
 
-		//can change the style of characters later
-		this.scorelabel = game.add.text(30,25,'Numbers: 0', {front : '10px Arial', fill: '#ffffff'});
-
-		this.lifelabel = game.add.text(game.world.width / 2 + 150, 25, 'Your Life: ',{front : '10px Arial', fill: '#ffffff'});
+		
 
 		//need to add the expload sound
 		this.explodeSound = game.add.audio('explode');
@@ -103,10 +109,10 @@ var level_1State = {
 		shape.outOfBoundsKill = true;
 
 		//set the speed of balloon
-		var minSpeed = -(Math.floor(Math.random() * 2));
-        var maxSpeed = Math.floor(Math.random() * 5);
-        var vx = Math.random()*(maxSpeed - minSpeed+1)-minSpeed;
-        var vy = Math.random()*(maxSpeed - minSpeed+1)-minSpeed;
+		var minSpeed = -(Math.floor(Math.random()));
+        var maxSpeed = Math.floor(Math.random() * 1.5);
+        var vx = Math.random() * (maxSpeed - minSpeed + 1) - minSpeed;
+        var vy = Math.random() * (maxSpeed - minSpeed + 1) - minSpeed;
 
         shape.body.velocity.y -= vy;
 
@@ -131,7 +137,7 @@ var level_1State = {
 
 		if(sprite.key == 'triangle'){
 			this.counterKill++;
-		}else {
+		} else {
 			this.lifes--;
 			this.heartsGroup.children[this.lifes].kill();
 		}
@@ -141,8 +147,8 @@ var level_1State = {
 		//call game over here directly
 		if (this.lifes == 0){
 			this.win = false;
+			this.messageGameOver = "You lose!"
 			this.gameOver();
-			this.messageGameOver = "You loose!"
 
 			
 		}
@@ -162,8 +168,37 @@ var level_1State = {
 //	},
 
 
-	showScoreBoardDead: function(){
+	showScoreBoardLose: function(){
+		this.gameOverLabel = game.add.text(game.world.width / 2,120, this.messageGameOver, {font : '50px Arial',
+																							fill: '#ffffff'});
+		this.gameOverLabel.anchor.setTo(0.5, 0.5);
 
+		this.lifeFinalScore = game.add.text(game.world.width / 2  - 110, 220,'Life Remaining: ', {font : '30px Arial',
+																								  fill: '#000000'});
+		this.finalScore = game.add.text(game.world.width / 2 - 110, 280, 'Triangles: ', {font : '30px Arial',
+																				   		 fill: '#000000'})
+
+		this.scoreBoardGroup.create(game.world.width / 2 - 225 , 150, "scoreboard");
+
+		this.buttonReload = game.add.sprite(game.world.width / 2 - 15, game.world.height / 2 + 75, "reload");
+		this.buttonReload.scale.setTo(0.1, 0.1)
+		this.buttonReload.inputEnabled = true;
+		
+
+		this.buttonReload.events.onInputDown.add(this.restartGame, this);
+		this.scoreBoardGroup.add(this.buttonReload);
+
+		this.scoreBoardGroup.add(this.lifeFinalScore);
+		this.scoreBoardGroup.add(this.finalScore);
+		this.scoreBoardGroup.add(this.gameOverLabel);
+
+		game.world.bringToTop(this.finalScore);
+		game.world.bringToTop(this.lifeFinalScore);
+
+		this.lifeFinalScore.text = "Life Remaining: " + this.lifes;
+		this.finalScore.text = "Triangles: " + this.counterKill;
+
+		game.add.tween(this.scoreBoardGroup).from( { y: -200 }, 2000, Phaser.Easing.Bounce.Out, true);
 
 	},
 
@@ -173,7 +208,7 @@ var level_1State = {
 
 	gameOver: function(){
 
-		if(this.win){
+		if (this.win){
 			this.bgSound.stop();
 			this.explodeSound.stop();
 			this.winSound.play();
@@ -181,13 +216,13 @@ var level_1State = {
 			game.time.events.stop();
 			this.showScorewin();
 
-		}else{
+		} else {
 			this.bgSound.stop();
 			this.explodeSound.stop();
 			this.loseSound.play();
 			this.loseSound.loop = false;
 			game.time.events.stop();
-			this.showScoreBoardDead();
+			this.showScoreBoardLose();
 		}
 
 	},
