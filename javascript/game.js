48fcffb7e4625fe267a348af3dd24a145c5d23f4
Siming Chen <susanchen1996@gmail.com>
2016-12-10 20:38:19 -0500

    debugged

--- javascript/game.js
+++ javascript/game.js
@@ -7,5 +7,6 @@ game.global = {
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('level_1', level_1State);
+game.state.add('mobile', mobileSate);
 
 game.state.start('boot');
