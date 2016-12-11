2977f67b050787d780bc0e0a3845212c8f4525f2
Siming Chen <susanchen1996@gmail.com>
2016-12-10 20:44:42 -0500

    test game state

--- javascript/game.js
+++ javascript/game.js
@@ -4,6 +4,7 @@ game.global = {
     score : 0
 };
 
+game.add.text( 100, 100, "Hello game is working", {front : '10px Arial', fill: '#ffffff'});
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('level_1', level_1State);
