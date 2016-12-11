987a27f7785332e2ebd031e290d8054c3ba873dd
xiaoyu-iid <xiaoyu.shi@outlook.edu>
2016-12-10 22:00:16 -0500

    scale life labels

--- javascript/mobile.js
+++ javascript/mobile.js
@@ -46,7 +46,7 @@ var mobileState = {
 
 		this.lifelabel = game.add.text(window.innerWidth * 0.03, window.innerHeight * 0.11, 'Your Lives: ',{front : '10px Arial',
 																					  fill: '#ffffff'});
-		this.lifelabel.scale.setTo(window.innerWidth / 480, window.innerWidth / 480);
+		this.lifelabel.scale.setTo(window.innerWidth / 550, window.innerWidth / 550);
 
 		//creating the three hearts in thce group and render on the screen
 		for (var i = 0; i < this.lifes; i++){
