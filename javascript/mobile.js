c90efec41dedad37455fa098aeab152fd819a2c6
xiaoyu-iid <xiaoyu.shi@outlook.edu>
2016-12-10 23:30:27 -0500

    scale scoreboard location

--- javascript/mobile.js
+++ javascript/mobile.js
@@ -233,7 +233,7 @@ var mobileState = {
 
 		this.finalScore.scale.setTo(window.innerWidth / 720, window.innerWidth / 720);
 
-		this.scoreBoardGroup.create((game.world.width / 2 - 225 * game.world.width / 640), window.innerHeight * 0.45, "scoreboardwin");
+		this.scoreBoardGroup.create(window.innerWidth / 720 * 0.3, window.innerHeight * 0.55, "scoreboardwin");
 		//console.log("gameworld" + game.world.width);
 		//console.log("gameworld" + game.world.width/2);
 		//console.log("gameworld" + (game.world.width/2 - 225 ));
@@ -242,7 +242,7 @@ var mobileState = {
 		//console.log("window" + (game.world.width / 2 - (225 * (game.world.width / 1144))));
 		//console.log ("should be" + (game.world.width / 2 - 225));
 
-		this.scoreBoardGroup.scale.setTo(game.world.width / 640, game.world.width / 640);
+		this.scoreBoardGroup.scale.setTo(game.world.width / 550, game.world.width / 550);
 		//this.scoreBoardGroup.position.x = (game.world.width / 2 - (450 * (game.world.width / 1144)));
 		//console.log (this.scoreBoardGroup.position.x);
 
