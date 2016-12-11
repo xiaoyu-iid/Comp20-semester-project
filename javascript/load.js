a6f42657a03d3b7cd963537b44c8879d412208c2
Siming Chen <susanchen1996@gmail.com>
2016-12-10 20:23:13 -0500

    test mobile

--- javascript/load.js
+++ javascript/load.js
@@ -24,7 +24,7 @@ var loadState = {
 	if(!game.device.desktop){
 			game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
-			document.body.style.backgroundColor = '#0000FF';
+			document.body.style.backgroundColor = '#000000';
 
 			game.state.start('mobile');
 
