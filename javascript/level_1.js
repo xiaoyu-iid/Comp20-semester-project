d88ca385a7917f9b5ac20c02e37f93bd5c2d2ad2
xiaoyu-iid <xiaoyu.shi@outlook.edu>
2016-12-11 21:09:55 -0500

    webpage debug

--- javascript/level_1.js
+++ javascript/level_1.js
@@ -261,7 +261,7 @@ var level_1State = {
 
 
 		//leaderboard button
-		this.buttonLeader = game.add.sprite(game.world.width/2 , game.world.height * 0.6, "leaderboard");
+		this.buttonLeader = game.add.sprite(game.world.width/2 , game.world.height * 0.55, "leaderboard");
 		this.buttonLeader.scale.setTo(window.innerWidth / 1080 * 0.45, window.innerWidth / 1080 * 0.45);
 		this.buttonLeader.inputEnabled = true;
 
